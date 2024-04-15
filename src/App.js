75417bf6bb41d36535861b76814bac19779700fa
Usmon <abdullaevusmon2006@gmail.com>
2024-04-15 16:06:54 +0500

    add GTM

--- src/App.js
+++ src/App.js
@@ -1,8 +1,35 @@
+import { useEffect } from "react";
+
 import Admin from "./pages/Admin";
 import Root from "./pages/Root";
 
+const GTM_ID = "GTM-MWJMHFPH";
+
 function App() {
+  useEffect(() => {
+    const initializeGTM = () => {
+      window.dataLayer = window.dataLayer || [];
+      function gtag() {
+        window.dataLayer.push(arguments);
+      }
+      gtag("js", new Date());
+      gtag("config", GTM_ID);
+    };
+
+    const script = document.createElement("script");
+    script.src = `https://www.googletagmanager.com/gtm.js?id=${GTM_ID}`;
+    script.async = true;
+    document.head.appendChild(script);
+
+    initializeGTM();
+
+    return () => {
+      document.head.removeChild(script);
+    };
+  }, []);
+
   if (window.location.pathname === "/admin") return <Admin />;
+
   return <Root />;
 }
 
