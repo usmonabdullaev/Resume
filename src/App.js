c0b0ace5355707920aaa0542cafac1c0a055790d
Usmon <abdullaevusmon2006@gmail.com>
2024-04-28 14:30:49 +0500

    add viewer

--- src/App.js
+++ src/App.js
@@ -1,5 +1,23 @@
+import { useEffect } from "react";
+import axios from "axios";
+
 import Root from "./pages/Root";
 
-const App = () => <Root />;
+const App = () => {
+  useEffect(() => {
+    const updateViews = async () => {
+      const viewsRes = await axios.get(
+        "https://e4ca4cd30c0b0fd8.mokky.dev/views"
+      );
+      const views = viewsRes.data[0].view + 1;
+      await axios.patch("https://e4ca4cd30c0b0fd8.mokky.dev/views/1", {
+        view: views,
+      });
+    };
+    updateViews();
+  }, []);
+
+  return <Root />;
+};
 
 export default App;
